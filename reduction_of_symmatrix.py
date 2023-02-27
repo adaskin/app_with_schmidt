36992010e12e418ea489d6f40db48ffde4537b6e
adaskin <3620717+adaskin@users.noreply.github.com>
2023-02-27 15:53:36 +0300

    Update reduction_of_symmatrix.py

--- reduction_of_symmatrix.py
+++ reduction_of_symmatrix.py
@@ -12,7 +12,8 @@ from sim_tree import generate_tree_elements,sum_of_nonzeropaths
 import sklearn.datasets as datasets
 import scipy.sparse
 import scipy.linalg  
-
+plt.use('PS') 
+plt.rcParams['text.usetex'] = True
 
 n = 10
 N = 2**n
